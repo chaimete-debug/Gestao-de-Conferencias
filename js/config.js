93e33fa4a2f17a01c20cfdbd790792857cfdafd7
chaimete-debug <chaimete@gmail.com>
2026-08-12 17:18:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_SETTINGS = {
   APP_NAME: 'Gestão da Conferência de Mulheres Nazarenas de Moçambique',
-  API_URL: 'COLE_AQUI_A_URL_DO_APPS_SCRIPT',
+  API_URL: 'https://script.google.com/macros/s/AKfycbx5xhqxSNHjlyzLDjn96y8QFyhx6JYe4L1cc1iXnxHRdH6bsunIIb_dyzu-CQrUtqIq/exec',
   SANDBOX: new URLSearchParams(window.location.search).get('sandbox') === '1',
   TOKEN_KEY: 'cmnm_session_token',
   USER_KEY: 'cmnm_current_user',
